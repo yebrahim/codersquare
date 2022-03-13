e8e4ba2355ab89a45e050f5d82c4e5fc9e03fc8a
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-03-13 09:29:16 -0400

    cleanup

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import express, { ErrorRequestHandler, RequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 
 import { initDb } from './datastore';
@@ -12,42 +11,35 @@ import {
   getPostHandler,
   listPostsHandler,
 } from './handlers/postHandler';
-import { authMiddleware } from './middleware/authMiddleware';
 import { errHandler } from './middleware/errorMiddleware';
-import { requestLoggerMiddleware } from './middleware/loggerMiddleware';
+import { loggerMiddleware } from './middleware/loggerMiddleware';
 
 (async () => {
   await initDb();
-  dotenv.config();
 
   const app = express();
 
   app.use(express.json());
 
-  app.use(requestLoggerMiddleware);
+  app.use(loggerMiddleware);
 
-  // Public endpoints
-  app.get('/healthz', (req, res) => res.send({ status: '✌️' }));
-  app.post('/v1/signup', asyncHandler(signUpHandler));
-  app.post('/v1/signin', asyncHandler(signInHandler));
-  // TODO: create signOutHandler
-
-  app.use(authMiddleware);
-
-  // Protected endpoints
-  app.get('/v1/posts', asyncHandler(listPostsHandler));
-  app.post('/v1/posts', asyncHandler(createPostHandler));
-  app.delete('/v1/posts', asyncHandler(deletePostHandler));
+  app.get('/v1/posts/list', asyncHandler(listPostsHandler));
+  app.post('/v1/posts/new', asyncHandler(createPostHandler));
+  app.delete('/v1/posts/:id', asyncHandler(deletePostHandler));
   app.get('/v1/posts/:id', asyncHandler(getPostHandler));
 
-  app.post('/v1/likes', asyncHandler(createLikeHandler));
+  app.post('/v1/signup', asyncHandler(signUpHandler));
+  app.post('/v1/signin', asyncHandler(signInHandler));
+
+  app.post('/v1/likes/new', asyncHandler(createLikeHandler));
   app.get('/v1/likes/:postId', asyncHandler(getLikesHandler));
 
-  app.post('/v1/comments', asyncHandler(createCommentHandler));
+  app.post('/v1/comments/new', asyncHandler(createCommentHandler));
+  //TODO: implement getCommentsHandler
+  //app.get('/v1/comments/list',asyncHandler(getCommentsHandler) )
   app.delete('/v1/comments/:id', asyncHandler(deleteCommentHandler));
-  // TODO: create listCommentsHandler
 
   app.use(errHandler);
 
-  app.listen(process.env.PORT || 3000);
+  app.listen(3000);
 })();
